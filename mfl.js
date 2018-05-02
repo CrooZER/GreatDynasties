88683c4b9855ec0ea2bc35c75731aedfefffc9d2
Artem Cheban <croozerua@gmail.com>
2018-05-02 23:44:40 +0300

    mfl

--- mfl.js
+++ mfl.js
@@ -1,6 +1,3 @@
-$(document).ready(
-function(){
-
   var MAP_YAHOO_FP = {'nfl': {},'mlb': {},'nba': {}};
 var MAP_ESPN_FP = {'nfl': {},'mlb': {},'nba': {}};
 var MAP_CBS_FP = {'nfl': {},'mlb': {},'nba': {}};
@@ -19,5 +16,27 @@ for (var i=0; i<NFL_PLAYER_DATA.players.length; i++){
 	if (p.rt){MAP_RT_FP['nfl'][String(p.rt)] = p.fp}
 	if (p.ff){MAP_FLEA_FP['nfl'][String(p.ff)] = p.fp}
 }
+
+$(document).ready(
+function(){
+jQuery("a").each(function(index){
+	    	var href = jQuery(this).prop("href");
+	    	if (href && href.indexOf){
+		    	var idx1 = href.indexOf("/player?");
+		    	var idx2 = href.indexOf("&P=");
+
+		    	if (idx1 != -1 && idx2 > idx1){
+		    		var mflId = href.substring(idx2 + "&P=".length);
+		    		while (mflId.length > 0 && mflId[0] == '0'){
+		    			mflId = mflId.substring(1);
+		    		}
+		    		 if (MAP_MFL_FP['nfl'][mflId]){
+		    		 	var fpId = MAP_MFL_FP['nfl'][mflId];
+		    		 	jQuery(this).addClass('fp-player-link').addClass('fp-id-' + fpId);
+		    		 }
+		    	} 
+	    	}
+	    });
+
 });
   
